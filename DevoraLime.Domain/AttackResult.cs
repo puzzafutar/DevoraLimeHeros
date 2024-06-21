96040bacb8f28ea5c3e978d95ab8ad2a672e64a7
czako <czakonorbert88@gmail.com>
2024-06-21 09:21:52 +0200

    Add some test for project

--- DevoraLime.Domain/AttackResult.cs
+++ DevoraLime.Domain/AttackResult.cs
@@ -14,7 +14,7 @@
 
         public string GetAfterAttackString()
         {
-            Hero winner = AttackHero.HealtPoint > AttackedHero.HealtPoint ? AttackHero : AttackedHero;
+            Hero winner = AttackHero.HealtPoint >= AttackedHero.HealtPoint ? AttackHero : AttackedHero;
 
             return $"Támadás után: Támadó: {AttackHero.Type} id: {AttackHero.Id} hp: {AttackHero.HealtPoint}, Védekező: {AttackedHero.Type} id: {AttackedHero.Id} hp: {AttackedHero.HealtPoint}, a győztes: {winner.Type} id: {winner.Id} ";
         }
---
+++ DevoraLimeHeros.DomainTests/AttackResultTests.cs
@@ -0,0 +1,36 @@
+﻿using DevoraLimeHeros.Application.Factory.Interface;
+using DevoraLimeHeros.ApplicationTests;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DevoraLimeHeros.Domain.Tests
+{
+    [TestClass()]
+    public class AttackResultTests : DITestBase
+    {
+        [TestMethod()]
+        public void GetAfterAttackString_ReturnsTrue()
+        {
+            IHeroFactory heroFactory = ServiceProvider.GetService<IHeroFactory>();
+
+            Hero archeryHero = heroFactory.GetHero(HeroTypeEnum.Archery, 1);
+            Hero riderHero = heroFactory.GetHero(HeroTypeEnum.Archery, 2);
+
+            AttackResult attackResult = new AttackResult(archeryHero, riderHero);
+
+            Assert.IsTrue(attackResult.GetAfterAttackString().Length > 10);
+        }
+
+        [TestMethod()]
+        public void GetBeforeAttackString_ReturnsTrue()
+        {
+            IHeroFactory heroFactory = ServiceProvider.GetService<IHeroFactory>();
+
+            Hero archeryHero = heroFactory.GetHero(HeroTypeEnum.Archery, 1);
+            Hero riderHero = heroFactory.GetHero(HeroTypeEnum.Archery, 2);
+
+            AttackResult attackResult = new AttackResult(archeryHero, riderHero);
+
+            Assert.IsTrue(attackResult.GetBeforeAttackString().Length > 10);
+        }
+    }
+}
